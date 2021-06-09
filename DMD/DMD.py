68c3e1dd025d7f252644126d20b4681795cddd2e
cristian1928 <cristian1928@ufl.edu>
2021-06-09 14:37:54 -0400

    Commented Program

--- DMD/DMD.py
+++ DMD/DMD.py
@@ -3,9 +3,11 @@ import numpy as np
 import math
 import os
 
+#Global Variables / Parameters
 MAX_POWER = 40
-debug = False
+debug = True
 
+#Program Variables
 store = False
 offset = 0
 xVal = 0
@@ -32,11 +34,16 @@ index = []
 if(not debug):
     print("Working...\n")
 
+#Opens Data Output File
 file = open(r"C:\Users\r.bowers\AppData\Roaming\ANTwareII\logs\Device0.txt","r+")
 
+#Iterate through each line in the file
 for line in file:
+    #only focus on lines with full data (82 characters)
     if len(line) == 82:
+        #iterate through each character in the line
         for character in line:
+            #filters out any characters that are not raw hex data
             if character == '[':
                 store = True
             if character == ']':
@@ -44,7 +51,9 @@ for line in file:
             if store and character != '[':
                 rawData.append(character)
 
+#iterates through raw data and creates pairs of hex data s 
 for i in range(0, int(len(rawData)/2)):
+    #format allows for leading zeroes in raw hex data to not be lost during int conversion (ex: 01)
    data.append(format(int(rawData[2*i] + rawData[(2*i)+1], 16), '#04x'))
 
 #Deletes File Contents After Opening
@@ -52,14 +61,14 @@ if(not debug):
     file.truncate(0)
 file.close()
 
+#Counts how many rows (data points) of data there exist 
 for i in range(0, len(data), 13):
    test.append(data[i:i+13])
-
 rowNum = len(test)
 
 for i in range(0, rowNum):
-    #Only if row data type is type '32'
-    #Takes corresponding hex data from long string of data
+    #Only if row data type is type '32' (Crank Torque Frequency Data)
+    #Takes corresponding hex data from long string of data, comes from documentation
     if int(data[13*i + 4], 16) == 32:
         eventCount.append(int(data[13*i + 5], 16))
         slope.append(int(data[13*i + 6] + data[13*i + 7][:0] + data[13*i + 7][2:], 16)*10)
@@ -98,6 +107,7 @@ for i in range(1, len(eventCount)):
         torqueFrequency = 1/(elapsedTime/torqueStamp) - offset
         torque.append(round(torqueFrequency/(slope[i]/10), 2))
 
+#Power Calculation
 for i in range(0, len(cadence)):
     power.append(round(cadence[i]*torque[i]*math.pi/30,2))
     if(debug):
@@ -111,7 +121,6 @@ for i in range(0, len(power)):
         index.append(i)
         if(debug):
             print("\n Power Removed has value " + str(power[i]) + " at index " + str(i))
-  
 for ele in sorted(index, reverse = True): 
     del power[ele]
     del time[ele]
@@ -130,6 +139,7 @@ if(debug):
 #        print("\yMax: " + str(yMax))
 #        print("\yMax: " + str(yMax))
 
+#Clear index List
 index.clear()
 
 #Remove Outliers
@@ -150,15 +160,19 @@ f = open(r"C:\Users\Public\DMD Project\Data\Data.csv","w+")
 f.close()
 
 count = 0
+
+#Writing Data to text file
 for i in range(0, len(power)):
     with open(r"C:\Users\Public\DMD Project\Data\Data.csv", "a") as fileToWrite:
         if count == 0:
             fileToWrite.write("Time , Power\n")
             count = count + 1
         fileToWrite.write(str(time[i]) + "," + str(power[i]) + "\n")
-       
+ 
+#Calculate Energy
 totalEnergy = round(np.trapz(power, time), 2)
 
+#Plot Data
 plt.title("Total Energy: " + str(totalEnergy))
 plt.xlabel('Time') 
 plt.ylabel('Power')
